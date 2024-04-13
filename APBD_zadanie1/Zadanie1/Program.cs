1a194ec1fd16307e4f50842f419f3ad5a3ab9ba0
Bartłomiej Kawa <kawaxbart@gmail.com>
2024-04-13 10:04:54 +0200

    Add the Average method

--- APBD_zadanie1/Zadanie1/Program.cs
+++ APBD_zadanie1/Zadanie1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hey, Universe!");
+
+static double Average(int[] numbers) => numbers.Average();
+
+Console.WriteLine(Average([2, 3, 5, -9]));
