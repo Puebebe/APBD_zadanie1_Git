22f546066c67ef1ff7b696b818043a50ee1f5ecc
Bartłomiej Kawa <kawaxbart@gmail.com>
2024-04-13 10:46:26 +0200

    Add the Max method

--- APBD_zadanie1/Zadanie1/Program.cs
+++ APBD_zadanie1/Zadanie1/Program.cs
@@ -4,3 +4,8 @@ Console.WriteLine("Hey, Universe!");
 static double Average(int[] numbers) => numbers.Average();
 
 Console.WriteLine(Average([2, 3, 5, -9]));
+
+static int Max(int[] numbers) => numbers.Max();
+
+Console.WriteLine(Max([2, 3, 5, -9]));
+
